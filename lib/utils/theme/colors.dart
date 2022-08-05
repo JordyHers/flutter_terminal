f82c830a024d45a593dd8663b39dcf83060477a3
goodluck wile <goodluckwileonline@gmail.com>
2022-08-05 22:57:31 +0300

    update color palett

--- lib/utils/theme/colors.dart
+++ lib/utils/theme/colors.dart
@@ -11,7 +11,10 @@ static  const kClear = Colors.deepOrangeAccent;
 
 
 //new colors pallete
-// ["#363535","#C45049","#66AF57","#8BBCC5","#4C36C0"]
-
-
+// {"Marigold":"f0a202","Tangerine":"f18805","Flame":"d95d39","Oxford Blue":"0e1428","Morning Blue":"7b9e89"}
+static const kMarigold = Color(0xfff0a202);
+static const kTangerine = Color(0xfff18805);
+static const kFlame = Color(0xffd95d39);
+static const kOxfordBlue = Color(0xff0e1428);
+static const kMorningBlue = Color(0xff7b9e89);
 }
